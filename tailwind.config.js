526851303e884a74b8244314e8e1f1f43b3236ee
marklim61 <mark.lim288580@gmail.com>
2024-05-01 02:18:29 -0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'ingrain-background': '#fbf1d8',
+      'ingrain-accent-color': '#f26a36',
+    },
+  },
+  plugins: [],
+}
+
