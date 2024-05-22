9b18ecc61eb4e021ad4e2633200e6e6d5eca3cf2
marklim61 <mark.lim288580@gmail.com>
2024-05-21 19:01:44 -0700

    updating about us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './Style.css'
 import YoutubeEmbed from '../components/YoutubeEmbed'
-import islandFood from '../assets/islandFoodBG.jpg'
+// import islandFood from '../assets/islandFoodBG.jpg'
 
 const About = () => {
   return (
@@ -22,9 +22,9 @@ const About = () => {
         <div 
           className="flex items-center bg-ingrain-board-color h-84 w-84 p-36 mt-72 rounded-3xl drop-shadow-2xl"
           style={{
-            backgroundImage: `url(${islandFood})`,
-            backgroundSize: 'cover', 
-            backgroundPosition: 'center' 
+            // backgroundImage: `url(${islandFood})`,
+            // backgroundSize: 'cover', 
+            // backgroundPosition: 'center' 
           }}>
           <article className="prose lg:prose-xl aesthet_nova text-center">
             <h1 className="aesthet-nova-h1 text-outline-white">The art of crafting cool food</h1>
@@ -41,34 +41,39 @@ const About = () => {
         </div>
       </div>
       
-
-      {/* <div className="flex justify-center items-center">
-        <div className="w-96 carousel carousel-horizontal rounded-box">
-          <div className="carousel-item h-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="image1"/>
+      <div className='flex justify-center items-center mt-60 bg-ingrain-board-color'>
+        <div>
+          <p className='aesthet-nova-h2'>Instagram</p>
+          <p className='aesthet-nova'>Photo Gallery</p>
+          <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">View More</button>
+        </div>
+        <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box w-screen">
+          <div className="carousel-item">
+            <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="img1" className="rounded-box" />
           </div> 
-          <div className="carousel-item h-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="image1"/>
+          <div className="carousel-item">
+            <img src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="img1" className="rounded-box" />
           </div> 
-          <div className="carousel-item h-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="image1"/>
+          <div className="carousel-item">
+            <img src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="img1" className="rounded-box" />
           </div> 
-          <div className="carousel-item h-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="image1"/>
+          <div className="carousel-item">
+            <img src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="img1" className="rounded-box" />
           </div> 
-          <div className="carousel-item h-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="image1"/>
+          <div className="carousel-item">
+            <img src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="img1" className="rounded-box" />
           </div> 
-          <div className="carousel-item h-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="image1"/>
+          <div className="carousel-item">
+            <img src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="img1" className="rounded-box" />
           </div> 
-          <div className="carousel-item h-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="image1"/>
+          <div className="carousel-item">
+            <img src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="img1" className="rounded-box" />
           </div>
         </div>
-      </div> */}
+      </div>
+      
 
-      <div className="flex justify-center items-center mt-36">
+      <div className="flex justify-center items-center m-36">
         <div className='size-9/12 drop-shadow-2xl'>
           <YoutubeEmbed embedId="bpOIp7EdRhg" />
         </div>
@@ -77,7 +82,7 @@ const About = () => {
       
       
       {/* Footer */}
-      <footer className="footer p-10 bg-base-200 text-base-content justify-evenly">
+      <footer className="footer p-10 text-base-content justify-evenly bg-ingrain-board-color">
         <nav>
           <h6 className="footer-title">Services</h6>
           <NavLink to="/events" className="link link-hover">Events</NavLink>
@@ -100,7 +105,7 @@ const About = () => {
         </nav>
       </footer>
 
-      <footer className="footer px-10 py-4 border-t bg-base-200 text-base-content border-base-300">
+      <footer className="footer px-10 py-4 border-t text-base-content border-base-300 bg-ingrain-board-color">
         <aside className="items-center grid-flow-col">
           <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
           <p>INGRAIN LLC<br/>The art of crafting cool food</p>
