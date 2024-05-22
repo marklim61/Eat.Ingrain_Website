ddbabe67cbdce6d5bdd057a4ff9357dde37168f0
nathanielflores <nathanielflores@Nathaniels-MBP.san.rr.com>
2024-05-21 17:02:58 -0700

    Daisyui to contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -94,30 +94,43 @@ const Contact = () => {
             <label className="block text-ingrain-color-green text-sm font-bold mb-2" htmlFor="name">
               Name
             </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              required
-            />
+            <label className="input input-bordered flex items-center gap-2">
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                className="grow"
+                placeholder="John Doe"
+                required
+              />
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70">
+                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+              </svg>
+            </label>
           </div>
           {/* Email input field */}
           <div className="mb-4">
             <label className="block text-ingrain-color-green text-sm font-bold mb-2" htmlFor="email">
               Email
             </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              required
-            />
+            <label className="input input-bordered flex items-center gap-2">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="grow"
+                placeholder="john.doe@gmail.com"
+                required
+              />
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70">
+                <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+              </svg>
+            </label>
           </div>
           {/* Message input field */}
           <div className="mb-6">
@@ -129,7 +142,8 @@ const Contact = () => {
               name="message"
               value={formData.message}
               onChange={handleChange}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="textarea textarea-bordered w-full"
+              placeholder="Message"
               rows="5"
               required
             />
