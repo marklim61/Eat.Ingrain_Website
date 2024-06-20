cc18cca71319b65d36470edfa9ce601c48f3b7e5
marklim61 <mark.lim288580@gmail.com>
2024-06-20 16:49:17 -0700

    updated footer for contact page

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -9,9 +9,9 @@ const Events = () => {
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
           </div>
           <div className="timeline-start md:text-end mb-10">
-            <time className="font-mono italic">1984</time>
-            <div className="text-lg font-black">First Macintosh computer</div>
-            The Apple Macintosh—later rebranded as the Macintosh 128K—is the original Apple Macintosh personal computer. It played a pivotal role in establishing desktop publishing as a general office function. The motherboard, a 9 in (23 cm) CRT monitor, and a floppy drive were housed in a beige case with integrated carrying handle; it came with a keyboard and single-button mouse.
+            <time className="font-mono italic">May 22 2024</time>
+            <div className="text-lg font-black aesthet-nova">Pure Project Miramar</div>
+              <p className='aesthet-nova-h2'>Celebrated Asian American and Pacific Islander Heritage Month. INGRAIN at @puremiramar <br></br>NEW MENU ITEMS & KICK OFF @purebrewing STRAWBERRY FEST RICE, BEER, HERITAGE</p>
           </div>
           <hr/>
         </li>
@@ -21,9 +21,9 @@ const Events = () => {
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
           </div>
           <div className="timeline-end mb-10">
-            <time className="font-mono italic">1998</time>
-            <div className="text-lg font-black">iMac</div>
-            iMac is a family of all-in-one Mac desktop computers designed and built by Apple Inc. It has been the primary part of Apple's consumer desktop offerings since its debut in August 1998, and has evolved through seven distinct forms
+            <time className="font-mono italic">November 18 2023</time>
+            <div className="text-lg font-black aesthet-nova">Night Parade Brewing Co.</div>
+              <p className='aesthet-nova-h2'>First ever plates at @nightparadebrewing</p>
           </div>
           <hr />
         </li>
